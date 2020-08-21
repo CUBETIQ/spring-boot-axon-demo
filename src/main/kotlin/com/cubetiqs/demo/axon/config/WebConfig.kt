61369fc2bfd973df0b7b5cd7db3d675887d07a3f
Sambo Chea <sombochea@cubetiqs.com>
2020-08-21 15:57:51 +0700

    Updated swagger ui config

---
+++ src/main/kotlin/com/cubetiqs/demo/axon/config/WebConfig.kt
@@ -0,0 +1,17 @@
+package com.cubetiqs.demo.axon.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+@EnableWebMvc
+class WebConfig : WebMvcConfigurer {
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("swagger-ui.html")
+            .addResourceLocations("classpath:/META-INF/resources/")
+        registry.addResourceHandler("/webjars/**")
+            .addResourceLocations("classpath:/META-INF/resources/webjars/")
+    }
+}
