652355f48684b79038508e6ff0a33dccffcab992
Sambo Chea <sombochea@cubetiqs.com>
2020-08-21 11:32:16 +0700

    Completed for api queries

--- src/main/kotlin/com/cubetiqs/demo/axon/api/AccountQueryController.kt
+++ src/main/kotlin/com/cubetiqs/demo/axon/api/AccountQueryController.kt
@@ -1,18 +1,28 @@
 package com.cubetiqs.demo.axon.api
 
 import com.cubetiqs.demo.axon.entity.BankAccount
+import com.cubetiqs.demo.axon.service.AccountQueryService
+import io.swagger.annotations.Api
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.CompletableFuture
 
-class AccountQueryController {
+@RestController
+@RequestMapping(path = ["/accounts"])
+@Api(value = "Bank Account Queries", description = "Bank Account Query Events API")
+class AccountQueryController @Autowired constructor(
+    private val accountQueryService: AccountQueryService
+) {
     @GetMapping("/{accountId}")
-    fun findById(@PathVariable("accountId") accountId: String?): CompletableFuture<BankAccount?>? {
+    fun findById(@PathVariable("accountId") accountId: String?): CompletableFuture<BankAccount?> {
         return this.accountQueryService.findById(accountId)
     }
 
     @GetMapping("/{accountId}/events")
-    fun listEventsForAccount(@PathVariable(value = "accountId") accountId: String?): List<Any?>? {
+    fun listEventsForAccount(@PathVariable(value = "accountId") accountId: String?): List<Any?> {
         return this.accountQueryService.listEventsForAccount(accountId)
     }
 }
